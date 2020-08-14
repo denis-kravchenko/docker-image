857d8c5f69c45803b141a962072c8d82f3049218
Denis Kravchenko <59100240+denis-kravchenko@users.noreply.github.com>
2020-08-14 16:34:09 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from K8S"</h1><br>";
+echo "Server IP Address is: $ip_server", "<br><cp>";
+echo "Made by <fint color=blue>Denis Kravchenko";
+
+?>
